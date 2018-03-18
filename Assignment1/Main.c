cf339700690972b356e8b6b1a5e32ee41e43ed89
Zachary Kastl <zkastl8@gmail.com>
2018-03-17 23:24:30 -0500

    added fibbonacci sequence

--- Assignment1/Main.c
+++ Assignment1/Main.c
@@ -5,13 +5,35 @@
 #include <time.h>
 
 typedef enum { false, true } BOOL;
-#define NUM_RANDS 10
+#define NUM_RANDS 10000000
 
 void Swap(int *a, int *b)
 {
 	int c = *a;
 	*a = *b;
 	*b = c;
+}
+
+int* RandomList()
+{
+	/* Initialize values */
+	srand((unsigned int)time(NULL));
+	int *list = (int*)malloc(NUM_RANDS * sizeof(int));
+
+	for (int idx = 0; idx < NUM_RANDS; idx++) {
+		list[idx] = rand() % 500;
+	}
+
+	return list;
+}
+
+int* CopyList(int* list)
+{
+	int* copy = (int*)malloc(NUM_RANDS * sizeof(int));
+	for (int i = 0; i < NUM_RANDS; i++) {
+		copy[i] = list[i];
+	}
+	return copy;
 }
 
 void PrintArray(int *A)
@@ -82,27 +104,65 @@ void Quicksort(int *A, int low, int high)
 	}
 }
 
-int main()
+unsigned long long* FibbonacciSequence(int num_fibs)
 {
-	/* Initialize values */
-	srand((unsigned int)time(NULL));
-	int *list = (int*)malloc(NUM_RANDS * sizeof(int));
-
-	for (int idx = 0; idx < NUM_RANDS; idx++) {
-		list[idx] = rand() % 500;
+	if (num_fibs < 2) {
+		printf("Not enough terms to generate a sequence; number of terms must be at least 2");
 	}
+	unsigned long long *fibs = (unsigned long long*)malloc(num_fibs * sizeof(unsigned long long));
+	fibs[0] = 1;
+	fibs[1] = 1;
 
+	printf("First %d Fibbonacci numbers: %d %d ", num_fibs, fibs[0], fibs[1]);
+	for (int i = 2; i < num_fibs; i++)
+	{
+		fibs[i] = fibs[i - 1] + fibs[i - 2];
+		printf("%d ", fibs[i]);
+	}
+	printf("\n");
+
+	return fibs;
+}
+
+void SortingAssignments()
+{
+	int* list = RandomList();
+	int* copy = CopyList(list);
+	
 	printf("Original list: ");
-	PrintArray(list);
-
+	/*PrintArray(list);	*/
 	time_t startTime = time(NULL);
-	/*Quicksort(list, 0, NUM_RANDS - 1);*/
-	BubbleSort(list, NUM_RANDS);
-	printf("%d seconds ", (int)(time(NULL) - startTime));
-
+	Quicksort(list, 0, NUM_RANDS - 1);
+	printf("List sorted in %d seconds \n", (int)(time(NULL) - startTime));
 	printf("Sorted List: ");
-	PrintArray(list);
+	/*PrintArray(list);	*/
+	printf("\n");
+
+	printf("Original list copy: ");
+	/*PrintArray(list);	*/
+	startTime = time(NULL);
+	BubbleSort(copy, NUM_RANDS);
+	printf("List sorted in %d seconds \n", (int)(time(NULL) - startTime));
+	printf("Sorted List: ");
+	/*PrintArray(list);	*/
+	printf("\n");
 
 	free(list);
+	free(copy);
+}
+
+void FibbonacciAssignments()
+{
+	int num_fibs;
+	printf("\nEnter the number of fibbonacci numbers to generate:\n");
+	scanf("%d", &num_fibs);
+	FibbonacciSequence(num_fibs);
+}
+
+int main()
+{
+	SortingAssignments();
+	FibbonacciAssignments();
+
 	return(0);
 }
